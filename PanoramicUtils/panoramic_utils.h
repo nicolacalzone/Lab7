4aba2c468a0dff5a0fedce68651d957a2a16d7c4
nicolacalzone <nicolacalzone14@gmail.com>
2023-05-21 23:20:15 +0200

    rm

--- PanoramicUtils/panoramic_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAB5__PANORAMIC__UTILS__H
-#define LAB5__PANORAMIC__UTILS__H
-
-#include <opencv2/core.hpp>
-
-cv::Mat cylindricalProj(const cv::Mat &image, const double angle);
-
-#endif // LAB5__PANORAMIC__UTILS__H
